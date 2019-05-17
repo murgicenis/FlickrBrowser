c843752a8038f8cd8d1796f9e754ecde7ca3f538
Murgic Enis <e.murgic@go-james.com>
2019-05-17 21:33:45 +0200

    Progress

--- app/src/main/java/com/murgic/flickrbrowser/GetRawData.java
+++ app/src/main/java/com/murgic/flickrbrowser/GetRawData.java
@@ -2,7 +2,6 @@ package com.murgic.flickrbrowser;
 
 import android.os.AsyncTask;
 import android.util.Log;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
